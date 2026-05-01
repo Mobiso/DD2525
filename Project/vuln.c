5de2eb5fee7aec6bcdf4b1b10cc6a6bb87d34c66
Mobiso <99.jensjonsson@gmail.com>
2026-05-01 17:29:42 +0200

    updated vuln to work with AFL and added some Notes

--- Project/vuln.c
+++ Project/vuln.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h> 
-int main(int argc, char* argv[]) {
-	if(argc < 2){
-		printf("Please give me a number between 0 and 5 (inclusive)!\n");
-		return 1;
-	}
+#include <unistd.h>
+
+int main() {
+
+	char guess[2];
+	read(STDIN_FILENO, guess, 2);
+    //can be found by giving an argument larger than 5 
 	char secret_string[] = "You were not meant to find this....";
-	
 	char words[6][10] = {"big","large","chungus","fatty","acid","thor"};
-	printf("You found the word: %s\n",words[atoi(argv[1])]);
+	printf("You found the word: %s\n",words[atoi(guess)]);
+	
 	return 0;
 }
